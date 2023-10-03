aeb5ffa79ab83a4e60936557bd92fa915a9a26bb
Spencer Wilfahrt <spencerwilf@yahoo.com>
2023-10-03 12:40:08 -0400

    dsc engine interface

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -14,5 +14,25 @@ pragma solidity ^0.8.19;
  *  - No governance
  *  - No fees
  *  - Only backed by wETH and wBTC
+ * 
+ * DSC system is always overcollateralized. Value of collateral will never be less than the backed value of DSC.
+ * 
+ * @notice This contract handles the core logic of the DSC system, including minting and redeeming DSC, as well as depositing and withdrawing collateral.
+ * @notice This contract is loosely based on the MakerDAO DSS (DAI) system.
  */
-contract DSCEngine {}
+contract DSCEngine {
+
+    function depositCollateralAndMintDSC() external {}
+
+    function depositCollateral() external {}
+
+    function redeemCollateralForDSC() external {}
+
+    function redeemCollateral() external {}
+
+    function burnDSC() external {}
+
+    function liquidate() external {}
+
+    function getHealthFactor() external view {}
+}
