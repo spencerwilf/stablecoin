b591f58d6a8d84c29026d14b6f65de93d7e7f480
Spencer Wilfahrt <spencerwilf@yahoo.com>
2023-10-04 10:38:56 -0400

    deploy script

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,14 +4,34 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {StableCoin} from "../src/StableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDSC is Script {
 
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
 
     function run() external returns(StableCoin, DSCEngine) {
-        vm.startBroadcast();
-        new StableCoin();
-        new DSCEngine();
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed,
+        address wbtcUsdPriceFeed,
+        address weth,
+        address wbtc,
+        uint256 deployerKey
+        ) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
+        StableCoin stableCoin = new StableCoin();
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(stableCoin));
+
+        stableCoin.transferOwnership(address(engine));
         vm.stopBroadcast();
+
+        return (stableCoin, engine);
     }
 }
